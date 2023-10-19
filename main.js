434583bec74fdfd026f956765a9bafaa55702423
Grzegorz <grzesiu9779120@wp.pl>
2023-10-19 12:12:00 +0200

    improvement(task1): Added lights to scene, refactored code

--- main.js
+++ main.js
@@ -1,78 +1,84 @@
 import * as THREE from "three";
 import { ParametricGeometry } from "three/addons/geometries/ParametricGeometry.js";
 
-const canvasContainer = document.getElementById("canvas-container");
-const width = canvasContainer.offsetWidth;
-const height = canvasContainer.offsetHeight;
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-camera.position.z = 25;
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(width, height);
-canvasContainer.appendChild(renderer.domElement);
+let scene;
+let camera;
+let renderer;
+let mesh;
 
 const controls = {
-  uValue: 1,
-  vValue: 1,
+  u: 1,
+  v: 1,
 };
 
-const parametricFunction = (u, v, target) => {
-  u *= Math.PI * 2 * controls.uValue;
-  v *= Math.PI * 2 * controls.vValue;
+function adjustLighting() {
+  const directionalLight = new THREE.DirectionalLight(0xdddddd);
+  directionalLight.position.set(-5, 3, 3);
+  scene.add(directionalLight);
+
+  let ambientLight = new THREE.AmbientLight(0xababab);
+  scene.add(ambientLight);
+}
+
+function parametricFunction(u, v, target) {
+  u *= Math.PI * 2 * controls.u;
+  v *= Math.PI * 2 * controls.v;
   target.set(u * Math.sin(v), u * Math.cos(v), v * Math.cos(u));
-};
+}
 
-const geometry = new ParametricGeometry(parametricFunction, 25, 25);
-const material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
-  side: THREE.DoubleSide,
-});
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
-const changeControlValue = (controlType, value) => {
-  if (controlType === "u") {
-    controls.uValue = value;
-  } else {
-    controls.vValue = value;
-  }
-};
-
-const updateGeometry = (controlType, value) => {
-  changeControlValue(controlType, value);
+function updateGeometry(controlType, value) {
+  controls[controlType] = value;
   const newGeometry = new ParametricGeometry(parametricFunction, 25, 25);
   mesh.geometry.dispose();
   mesh.geometry = newGeometry;
-  renderer.render(scene, camera);
-};
+}
 
-const animate = () => {
+function animate() {
   window.requestAnimationFrame(animate);
   mesh.rotation.x += 0.0025;
   mesh.rotation.y += 0.0025;
   renderer.render(scene, camera);
-};
+}
 
-const addSlidersLogic = () => {
-  document.addEventListener("DOMContentLoaded", () => {
-    const uSlider = document.getElementById("u-slider");
-    const vSlider = document.getElementById("v-slider");
+function addSlidersLogic() {
+  const uSlider = document.getElementById("u-slider");
+  const vSlider = document.getElementById("v-slider");
 
-    uSlider.addEventListener("input", (e) => {
-      updateGeometry("u", parseFloat(e.target.value));
-    });
-
-    vSlider.addEventListener("input", (e) => {
-      updateGeometry("v", parseFloat(e.target.value));
-    });
+  uSlider.addEventListener("input", (e) => {
+    updateGeometry("u", parseFloat(e.target.value));
   });
-};
 
-const main = () => {
+  vSlider.addEventListener("input", (e) => {
+    updateGeometry("v", parseFloat(e.target.value));
+  });
+}
+
+function init() {
+  const canvasContainer = document.getElementById("canvas-container");
+  const width = canvasContainer.offsetWidth;
+  const height = canvasContainer.offsetHeight;
+
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+  camera.position.z = 25;
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(width, height);
+  canvasContainer.appendChild(renderer.domElement);
+
+  const geometry = new ParametricGeometry(parametricFunction, 25, 25);
+  const material = new THREE.MeshPhongMaterial({
+    color: 0x0095dd,
+    shininess: 100,
+    side: THREE.DoubleSide,
+  });
+
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+
   addSlidersLogic();
+  adjustLighting();
   animate();
-};
+}
 
-main();
+init();
